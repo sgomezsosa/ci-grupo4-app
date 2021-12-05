5934778b287f9d492322bd87d3832d75ff105f37
Oscar Garzon <OscarJacinto.GarzonCubillos@scotiabank.com>
2021-12-05 10:39:48 -0500

    cambios de Hotfix
    Se actualiza cambios para corregir bug en produccion

--- src/test/java/com/master/grupo4/ApplicationTests.java
+++ src/test/java/com/master/grupo4/ApplicationTests.java
@@ -7,7 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoadsprimary() {
+	}
+
+	@Test
+	void systemtest() {
 	}
 
 }
