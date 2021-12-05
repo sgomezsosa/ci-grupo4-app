8b7888e3ca0416be58787c17d9641ce0dab6f9d4
ext_sgomezs <ext_sgomezs@falabella.cl>
2021-12-05 19:50:23 -0300

    [feature_sgomezs] | Samuel | Rename mainAppValue test method to newMainAppValue.

--- src/test/java/com/master/grupo4/ApplicationTests.java
+++ src/test/java/com/master/grupo4/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
 	@Test
-	void mainAppValue() {}
+	void newMainAppValue() {}
 
 	@Test
 	void contextLoads() {
